fabc4debd612c6e32fc9685e97f05f902e4195f6
Hatice Saka <haticesaka939@gmail.com>
2022-04-30 12:43:08 +0300

    30.04

--- src/day05_matematikselIslemler/C03_RakamlarToplaminiBulma.java
+++ src/day05_matematikselIslemler/C03_RakamlarToplaminiBulma.java
@@ -6,26 +6,28 @@ public class C03_RakamlarToplaminiBulma {
     public static void main(String[] args) {
         // kullanicidan aldiginiz 4 basamakli bir sayinin
         // basamaklardaki rakamlar toplamini bulunuz
-        Scanner scan= new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("lutfen 4 basamakli bir tam sayi giriniz");
-        int sayi=scan.nextInt();
-        int rakam=0;
-        int rakamlarToplami=0;
+        int sayi = scan.nextInt();
+        int rakam = 0;
+        int rakamlarToplami = 0;
         // 1. adim r=0 rt=0 sayi=7532
-        rakam=sayi%10; //2
-        rakamlarToplami +=rakam; // 2
-        sayi/=10; // 753
+        rakam = sayi % 10; //2
+        rakamlarToplami += rakam; // 2
+        sayi /= 10; // 753
         // 2. adim r=2 rt=2 sayi 753
-        rakam=sayi%10; // 3
+        rakam = sayi % 10; // 3
         rakamlarToplami += rakam; // 5
-        sayi /=10; // 75
+        sayi /= 10; // 75
         // 3. adim r=3 rt = 5 sayi=75
-        rakam=sayi%10; // 5
+        rakam = sayi % 10; // 5
         rakamlarToplami += rakam; // 10
         sayi /= 10; // 7
-        rakam=sayi%10; // 7
-        rakamlarToplami+=rakam; // 17
-        sayi/=10; // 7/10=> 0.7     0
+        rakam = sayi % 10; // 7
+        rakamlarToplami += rakam; // 17
+        sayi /= 10; // 7/10=> 0.7     0
         System.out.println("girdiginiz sayinin rakamlar toplami  " + rakamlarToplami);
+
     }
+
 }
---
+++ src/karalamaDefterim/For.java
@@ -0,0 +1,12 @@
+package karalamaDefterim;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 1; i < 4; i++) {
+            for (int j = 1; j < 4; j++) {
+                System.out.println(j*i);
+            }
+        }
+
+    }
+}
